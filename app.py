cf15edc9c2b303ab49f01e709cc07ae5ad054afd
RoeiCohen2212 <roeicohen49@walla.com>
2026-02-04 19:56:32 +0200

    Styled message for version 2

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello from version 1"
+    return  """
+<h1 style='color: #2a4dff; font-family: Arial; text-align: center; margin-top: 50px;'>
+    Welcome to Version 2 🚀
+</h1>
+<p style='font-size: 20px; text-align: center; color: #333;'>
+    ה־CI/CD שלך עובד בצורה מושלמת!
+</p>
+"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
